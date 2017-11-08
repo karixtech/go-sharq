0b5879e0ad3b43d97ffad968b553b3efe259510a
Kunal Kerkar <me@tsudot.com>
2017-11-08 14:58:04 +0530

    API for bulk enqueue built in along with tests

--- sharq.go
+++ sharq.go
@@ -3,9 +3,13 @@ package sharq
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"sync"
 )
 
 const (
@@ -18,16 +22,25 @@ type EnqueueRequest struct {
 	Payload  interface{} `json:"payload"`
 }
 
+type BulkEnqueueRequest struct {
+	JobID     string      `json:"job_id"`
+	Interval  int         `json:"interval"`
+	Payload   interface{} `json:"payload"`
+	QueueID   string      `json:"queue_id"`
+	QueueType string      `json:"queue_type"`
+}
+
 type EnqueueResponse struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
+	JobID   string `json:job_id"`
 }
 
 type Client struct {
 	// HTTP client used to communicate with the API.
 	client *http.Client
 
-	// Base URL for API requests. This should always be specified with the trailing slash.
+	// Base URL for API requests. This should always be specified without the trailing slash.
 	BaseURL *url.URL
 }
 
@@ -43,43 +56,100 @@ func NewClient(URL string) *Client {
 
 }
 
-func (c *Client) Enqueue(e *EnqueueRequest, queueType string, queueID string) (*EnqueueResponse, error) {
-	enqueueURL, err := url.Parse(fmt.Sprintf(c.BaseURL.String() + "/enqueue/" + queueType + "/" + queueID + "/"))
+func (c *Client) BulkEnqueue(e []BulkEnqueueRequest) ([]EnqueueResponse, error) {
+
+	queueMessages := make(chan EnqueueResponse)
+
+	var wg sync.WaitGroup
+
+	go monitorWorker(&wg, queueMessages)
+
+	// Add the number of requests to the WG
+	wg.Add(len(e))
+
+	for _, eRequest := range e {
+		go c.queueToSharq(eRequest, &wg, queueMessages)
+	}
+
+	var tempR []EnqueueResponse
+
+	for response := range queueMessages {
+		tempR = append(tempR, response)
+	}
+
+	return tempR, nil
+
+}
+
+func (c *Client) queueToSharq(e BulkEnqueueRequest, wg *sync.WaitGroup, cs chan EnqueueResponse) {
+	defer wg.Done()
+
+	er := &EnqueueRequest{JobID: e.JobID, Interval: e.Interval, Payload: e.Payload}
+	enqueueResponse, err := c.Enqueue(er, e.QueueType, e.QueueID)
 
 	if err != nil {
-		fmt.Printf("%v", err)
+		log.Fatalln(err)
+	}
+
+	cs <- enqueueResponse
+
+}
+
+func monitorWorker(wg *sync.WaitGroup, cs chan EnqueueResponse) {
+	wg.Wait()
+	close(cs)
+}
+
+func (c *Client) Enqueue(e *EnqueueRequest, queueType string, queueID string) (EnqueueResponse, error) {
+	var aResp EnqueueResponse
+
+	enqueueURL, err := url.Parse(fmt.Sprintf(c.BaseURL.String() + "/enqueue/" + queueType + "/" + queueID + "/"))
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 	jsonValue, err := json.Marshal(e)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
-	}
-
-	fmt.Println(string(jsonValue))
-
-	if err != nil {
-		fmt.Printf("%v", err)
-		return nil, err
+		return aResp, err
 	}
 
 	req, err := http.NewRequest("POST", enqueueURL.String(), bytes.NewBuffer(jsonValue))
-
 	if err != nil {
-		return nil, err
+		fmt.Printf("Error creating request object: %v", err)
+		return aResp, err
 	}
 
 	req.Header.Add("Content-Type", "application/json")
 
-	aResp := &EnqueueResponse{}
-
 	resp, err := c.client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+		return aResp, err
+	}
 
 	defer resp.Body.Close()
 
-	err = json.NewDecoder(resp.Body).Decode(aResp)
+	if resp.StatusCode == http.StatusCreated {
 
-	return aResp, err
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			return aResp, err
+		}
 
+		err = json.Unmarshal(bodyBytes, &aResp)
+		if err != nil {
+			fmt.Println(err)
+			return aResp, err
+		}
+		aResp.JobID = e.JobID
+	} else {
+		return aResp, errors.New("Could not create")
+	}
+
+	return aResp, nil
 }
 
 // func (c *Client) Dequeue(queueType string) (*DequeueResponse, error) {
--- sharq_test.go
+++ sharq_test.go
@@ -1,6 +1,10 @@
 package sharq
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+)
 
 const (
 	URL = "http://127.0.0.1:8080/api/v1/namespaces/default/services/sharq-server-sharq-server-chart:8081/proxy"
@@ -9,7 +13,7 @@ const (
 func TestEnqueue(t *testing.T) {
 	client := NewClient(URL)
 
-	er := &EnqueueRequest{JobID: "123-123", Interval: 4, Payload: map[string]string{"hello": "world"}}
+	er := &EnqueueRequest{JobID: "123-123", Interval: 4, Payload: map[string]string{"hello": "world", "foo": "bar"}}
 
 	enqueueResponse, err := client.Enqueue(er, "sms", "1")
 	if err != nil {
@@ -17,4 +21,23 @@ func TestEnqueue(t *testing.T) {
 	} else {
 		t.Logf("Enqueued: %v\n", enqueueResponse)
 	}
+}
+
+func TestBulkEnqueue(t *testing.T) {
+
+	client := NewClient(URL)
+
+	ber := []BulkEnqueueRequest{
+		{JobID: "134-145", Interval: 4, Payload: map[string]string{"hello": "world", "foo": "bar"}, QueueType: "sms", QueueID: "1"},
+		{JobID: "136-147", Interval: 4, Payload: map[string]string{"egg": "spam", "foo": "bar"}, QueueType: "sms", QueueID: "1"},
+	}
+
+	enqueueResponse, err := client.BulkEnqueue(ber)
+
+	if err != nil {
+		t.Errorf("Failed to queue: %v", err)
+	} else {
+		t.Logf("Enqueued: %v\n", enqueueResponse)
+	}
+	spew.Dump(enqueueResponse)
 }
